c0e6a5f09df77329f3488bcde36e5c6aacde9149
lsh150446 <103585029+lsh150446@users.noreply.github.com>
2026-03-23 15:21:10 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'AIMself';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '从 0 到 1 的笔记：产品、写作与自我进化。';
